0586d0c98985b3fe8e7db8e8fdfc8e6c958169de
jmacias <j.macias@paralleldevs.com>
2019-10-01 21:27:18 -0600

    calc ready

--- calc-js/js/scripts.js
+++ calc-js/js/scripts.js
@@ -1,62 +1,71 @@
 const numberList = document.getElementsByClassName('numbers__item');
 let view = document.getElementsByClassName('view');
 let number1 = "";
-let number2;
-let btnClicked;
+let number2 = "";
+let btnClicked = "";
 let currentOperator = "";
-let operator = "";
 
 const cleanView = () => {
   view[0].value = 0;
   number1 = "";
-  number2 = undefined;
-  btnClicked = undefined;
+  number2 = "";
+  btnClicked = "";
   currentOperator = "";
   operator = "";
 };
 
-const printOnView = (e) => {
-  btnClicked = e.target.innerText;
-
-  if (btnClicked !== "/" && btnClicked !== "+" && btnClicked !== "-" &&  btnClicked !== "x" &&  btnClicked !== "=" &&  btnClicked !== "C" &&  currentOperator == "") {
-    number1 = number1 + btnClicked;
-    view[0].value = number1;
-  } else if (number1 !== "" && number2 == undefined) {
-    currentOperator = btnClicked;
-    operator = btnClicked;
-    number2 = "";
-    view[0].value = currentOperator;
-  } else if (currentOperator === "/" || currentOperator === "+" || currentOperator === "-" ||  currentOperator === "x") {
-    number2 = number2 + btnClicked;
+const printCalc = (val = null) => {
+  view[0].value = val || number1 + " " + currentOperator + " " + number2; 
+  if (val) {
+    number1 = val;
     currentOperator = "";
-    view[0].value = number2;
+    number2 = "";
   }
 }
 
-const equal = (num1,num2) => {
-  console.log(operator);
-  switch (operator) {
-    case "/":
-      console.log("entró a dividir");
-      parseInt(number1)
-      parseInt(number2)
+const printOnView = (e) => {
+  btnClicked = e.target.innerText;
+  const operators = ["/","+","-","x"];
+  const allNumbers = ["0","1","2","3","4","5","6","7","8","9"];
+  
+  if (!operators.includes(btnClicked) && allNumbers.includes(btnClicked)) {
+    if (currentOperator) {
+      number2 = number2 + btnClicked;
+    } else  {
+      number1 = number1 + btnClicked;
       console.log(number1);
-      const division = () => {
-        let operation = parseInt(number1 / number2);
-        console.log(operation);
-      }
+    }
+    printCalc();
+  } 
+  
+  if (!allNumbers.includes(btnClicked) && operators.includes(btnClicked) && !currentOperator && number1) {
+    currentOperator = btnClicked;
+    printCalc();
+  }
+
+  if (btnClicked === "=") {
+    equal();
+  }
+}
+
+const equal = () => {
+  number1 = Number(number1);
+  number2 = Number(number2);
+  switch (currentOperator) {
+    case "/":
+      printCalc(number1 / number2);
+    break;
+    case "+":
+      printCalc(number1 + number2);   
       break;
-    // case "+":
-    //   operation(number1, +, number2);    
-    //   break;
-    // case "-":
-    //   operation(number1, -, number2);    
-    //   break;
-    // case "x":
-    //   operation(number1, *, number2);   
-    //   break;
+    case "-":
+      printCalc(number1 - number2);   
+      break;
+    case "x":
+      printCalc(number1 * number2);   
+      break;
     default:
-        console.log("not operator");
+      console.log("not operator");
       break;
   }
 }
@@ -65,9 +74,4 @@ for (let i = 0; i < numberList.length; i++) {
   numberList[i].addEventListener("click", printOnView );
 }
 
-numberList[0].addEventListener("click", cleanView );
-numberList[19].addEventListener("click", equal );
-// numberList[3].addEventListener("click", currentOperator );
-// numberList[7].addEventListener("click", currentOperator );
-// numberList[11].addEventListener("click", currentOperator );
-// numberList[15].addEventListener("click", currentOperator );
+numberList[0].addEventListener("click", cleanView);
