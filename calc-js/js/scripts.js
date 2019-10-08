a06bac27e0532261fdd07606e28919f1517d236d
jmacias <j.macias@paralleldevs.com>
2019-10-07 21:06:27 -0600

    fix ident

--- calc-js/js/scripts.js
+++ calc-js/js/scripts.js
@@ -1,5 +1,5 @@
-const numberList = document.getElementsByClassName('numbers__item');
-let view = document.getElementsByClassName('view');
+const numberList = document.getElementsByClassName("numbers__item");
+let view = document.getElementsByClassName("view");
 let number1 = "";
 let number2 = "";
 let btnClicked = "";
@@ -15,30 +15,30 @@ const cleanView = () => {
 };
 
 const printCalc = (val = null) => {
-  view[0].value = val || number1 + " " + currentOperator + " " + number2; 
+  view[0].value = val || number1 + " " + currentOperator + " " + number2;
   if (val) {
     number1 = val;
     currentOperator = "";
     number2 = "";
   }
-}
+};
 
-const printOnView = (e) => {
+const printOnView = e => {
   btnClicked = e.target.innerText;
-  const operators = ["/","+","-","x"];
-  const allNumbers = ["0","1","2","3","4","5","6","7","8","9"];
-  
+  const operators = ["/", "+", "-", "x"];
+  const allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
   if (!operators.includes(btnClicked) && allNumbers.includes(btnClicked)) {
     if (currentOperator) {
       number2 = number2 + btnClicked;
-    } else  {
+    } else {
       number1 = number1 + btnClicked;
       console.log(number1);
     }
     printCalc();
-  } 
-  
-  if (!allNumbers.includes(btnClicked) && operators.includes(btnClicked) && !currentOperator && number1) {
+  }
+
+  if ( !allNumbers.includes(btnClicked) && operators.includes(btnClicked) && !currentOperator && number1 ) {
     currentOperator = btnClicked;
     printCalc();
   }
@@ -46,7 +46,7 @@ const printOnView = (e) => {
   if (btnClicked === "=") {
     equal();
   }
-}
+};
 
 const equal = () => {
   number1 = Number(number1);
@@ -54,24 +54,24 @@ const equal = () => {
   switch (currentOperator) {
     case "/":
       printCalc(number1 / number2);
-    break;
+      break;
     case "+":
-      printCalc(number1 + number2);   
+      printCalc(number1 + number2);
       break;
     case "-":
-      printCalc(number1 - number2);   
+      printCalc(number1 - number2);
       break;
     case "x":
-      printCalc(number1 * number2);   
+      printCalc(number1 * number2);
       break;
     default:
       console.log("not operator");
       break;
   }
-}
+};
 
 for (let i = 0; i < numberList.length; i++) {
-  numberList[i].addEventListener("click", printOnView );
+  numberList[i].addEventListener("click", printOnView);
 }
 
 numberList[0].addEventListener("click", cleanView);
