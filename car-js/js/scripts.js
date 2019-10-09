7f17cecb5d075b9bc5e92046a521795c3f8205fe
jmacias <j.macias@paralleldevs.com>
2019-10-08 23:55:30 -0600

    open and close all doors

--- car-js/js/scripts.js
+++ car-js/js/scripts.js
@@ -5,16 +5,27 @@ const doorBtn = document.getElementById('doorBtn');
 const backBtn = document.getElementById('backBtn');
 const roofBtn = document.getElementById('roofBtn');
 const frontBtn = document.getElementById('frontBtn');
+const doors = document.getElementById('doors');
+const back = document.getElementById('back');
+const roof = document.getElementById('roof');
+const front = document.getElementById('front');
 const car = document.getElementById('car');
-// const dataPart = ['door','back','front', 'roof'];
+const engine = document.getElementById('engine');
 
-
-let isBlock = true;
-let currentDoor = '';
+var isBlock = true;
+var currentDoor = '';
+var doorState = {
+  doorsOpen: false,
+  backOpen: false,
+  fronOpen: false,
+  roofOpen: false,
+}
+var engineOn = false;
 
 const selectDoor = e => {
-  currentDoor = e.target.getAttribute('data-part');
-  console.log('data = ' + currentDoor);
+  if (isBlock === false) {
+    e.target.classList.toggle('active')
+  }
 }
 
 const blockCar = () => {
@@ -25,34 +36,62 @@ const blockCar = () => {
 const unblockCar = () => {
   control.classList.remove('blocked');
   isBlock = false
-  console.log('data = ' + currentDoor);
 };
 
-const openCar = () => {
+const openCar = (e) => {
+  currentDoor = e.target.getAttribute('data-part');
   if (isBlock === true) {
     alert('The car is blocked');
   }
 
   if (isBlock === false) {
-    openDoor()
-    console.log('Now is open', currentDoor)
-  } 
+    openDoor(doorState)
+  }
 };
 
-const openDoor = () => {
-  switch (currentDoor) {
-    case 'door':
-      console.log('open the door')
-      break;
-  
-    default:
-      break;
+const openClose = (stateDoor, part, indicator) => {
+  const carPart = `${part}Open`;
+  if (!stateDoor) {
+    doorState[carPart] = true;
+    indicator.classList.add('active')
+  } else {
+    doorState[carPart] = false;
+    indicator.classList.remove('active')
   }
 }
+
+const openDoor = (doorState) => {
+  switch (currentDoor) {
+    case 'door':
+      openClose(doorState.doorsOpen, 'doors', doors);
+    break;
+    case 'back':
+        openClose(doorState.backOpen, 'back', back);
+    break;
+    case 'front':
+        openClose(doorState.frontOpen, 'front', front);
+    break;
+    case 'roof':
+      openClose(doorState.roofOpen, 'roof', roof);
+    break;
+    default:
+    break;
+  }
+}
+
+const onOf = () => {
+  engine.classList.toggle('active');
+  if (!engineOn) {
+    engineOn = true;
+  } else {
+    engineOn = false; 
+  }
+};
 
 car.addEventListener('click', selectDoor);
 controlBlock.addEventListener('click', blockCar);
 controlUnblock.addEventListener('click', unblockCar);
+engine.addEventListener('click', onOf);
 doorBtn.addEventListener('click', openCar);
 backBtn.addEventListener('click', openCar);
 roofBtn.addEventListener('click', openCar);
